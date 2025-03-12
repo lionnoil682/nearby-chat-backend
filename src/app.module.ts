a231df1c8ff4b131f43c4a1ca1b284a904fb88c0
lionnoil682 <lionnoil682@gmail.com>
2025-03-12 15:17:31 +0900

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { join } from 'path';
+import 'dotenv/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host:
-        process.env.CLOUDSQL_HOST ||
-        `/cloudsql/${process.env.CLOUDSQL_INSTANCE_CONNECTION_NAME}`,
+        process.env.CLOUDSQL_HOST || process.env.LOCAL_DB_HOST || 'localhost',
       port: 5432,
       username: process.env.CLOUDSQL_USER,
       password: process.env.CLOUDSQL_PASS,
       database: process.env.CLOUDSQL_DB,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: process.env.NODE_ENV !== 'production',
     }),
     UsersModule,
   ],
